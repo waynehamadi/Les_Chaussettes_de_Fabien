f8d721b72e34386ca29317646a1058f335c8c93f
Merwane HAMADI <merwanehamadi@gmail.com>
2019-05-24 16:04:04 +0200

    better-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,44 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.new(email: "merwanehamadi2@gmail.com", password:"toto75")
-user1.save
+users = []
+5.times do
+  user = User.create(email: Faker::Internet.email, password: Faker::Lorem.characters(7))
+  users << user
+end
+socks = []
+5.times do
+  socks << Sock.new(title:"Amazing socks",
+  description:Faker::Lorem.sentence(10),
+  category: ["kid", "adult"].sample,
+  price: [2,3,4,5, 6,1].sample,
+  size: ["42-44", "40-42", "38-40", "44-46"].sample,
+  color: ["red", "blue", "yellow", "black"].sample)
+end
 
-url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
-sock1 = Sock.new(title:"Amazing socks",
-  description:"These socks smell like heaven",
-  category: "adult",
-  price: 2,
-  size: "42-44",
-  color: "red")
-sock1.user_id = user1.id
-sock1.remote_photo_url = url
-sock1.save
+socks.each do |sock|
+  sock.user_id = users.sample.id
+end
+urls = []
+urls << "https://static1.squarespace.com/static/558d8f8ee4b0e58af1aca873/55a95c71e4b0f765fc4497ac/5be5f765032be4400f2fed89/1541797756629/Background+copy+4.jpg"
+urls << "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxNQcbJ501SBsHmQMOtt1dE6a6udGQXKWmwKOnHY2EYmB16qkk6A"
+urls << "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm1nkd7lj0mdPB6cOKptnEAQjhMjOU-aibXwp6xjasnxWxs0vVWw"
+urls << "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNYzKFAXt-_meDhqVEyyD4-vMoubVjwLAKmrvGhrdaDGpE4LrX"
+urls << "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiW9JhjbsUGCkThkDu9KZOcjefOwr37-OHop34NscLGgtW1ya56g"
+
+
+socks.each_with_index do |sock, index|
+    sock.remote_photo_url = urls[index]
+    sock.save
+end
 
 booking = Booking.new()
 booking.sock_id = 1
 booking.user_id = 1
+booking.start_at = "01/06/2019"
+booking.end_at = "06/06/2019"
 booking.save
 review = Review.new(rating: 3, content: "bof")
 review.booking_id = 1
+
 review.save
