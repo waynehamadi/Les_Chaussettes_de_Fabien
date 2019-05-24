100ea833a272bbce8c32930482f6ca45e78b4b2f
Merwane HAMADI <merwanehamadi@gmail.com>
2019-05-24 13:03:36 +0200

    bug-fix-seed

--- db/migrate/20190524091209_add_columns_to_booking.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToBooking < ActiveRecord::Migration[5.2]
-  def change
-    add_column :bookings, :start_at, :date
-    add_column :bookings, :end_at, :date
-    add_reference :bookings, :user, foreign_key: true
-    add_reference :bookings, :sock, foreign_key: true
-  end
-end
