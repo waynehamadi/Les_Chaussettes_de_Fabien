2a95fc6a133be19ba7932cd22a609de5f179b39a
Merwane HAMADI <merwanehamadi@gmail.com>
2019-05-29 12:54:27 +0200

    add-a-review

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get 'socks/mysocks', to: 'socks#mysocks'
   resources :socks do
     resources :bookings, only: [:create]
-    resources :reviews, only: [:new]
   end
   get 'bookings/myrents', to: 'bookings#myrents'
-  resources :bookings, only: [:index, :show]
+  resources :bookings, only: [:index, :show] do
+    resources :reviews, only: [:new, :create]
+  end
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
