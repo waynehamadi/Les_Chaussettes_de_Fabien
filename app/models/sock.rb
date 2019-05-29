66f4265ff14c52c73ec5e08b4b67b8dc47c48a7c
fabien rizzoli <fabien.rizzoli@gmail.com>
2019-05-29 13:26:34 +0200

    added a form

--- app/models/sock.rb
+++ app/models/sock.rb
@@ -1,8 +1,9 @@
 class Sock < ApplicationRecord
-
   mount_uploader :photo, PhotoUploader
   belongs_to :user
   validates :title, presence: true
+  validates :price, presence: true
+  validates :photo, presence: true
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
+
   get 'socks/mysocks', to: 'socks#mysocks'
+
   resources :socks do
     resources :bookings, only: [:create]
   end
+
   get 'bookings/myrents', to: 'bookings#myrents'
+
   resources :bookings, only: [:index, :show]
+
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
